0b6027c9f0da3490adcf74379516bb43ce93beac
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-10-09 00:19:32 +0900

    use ES6 in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
-var gulp = require('gulp');
-var frontMatter = require('gulp-front-matter');
-var markdown = require('gulp-markdown');
-var layout = require('gulp-layout');
-var htmlmin = require('gulp-htmlmin');
+const gulp = require('gulp');
+const frontMatter = require('gulp-front-matter');
+const markdown = require('gulp-markdown');
+const layout = require('gulp-layout');
+const htmlmin = require('gulp-htmlmin');
 
-gulp.task('default', function() {
+gulp.task('default', () => {
   gulp.src('src/*.md')
     .pipe(frontMatter({remove: true}))
     .pipe(markdown())
-    .pipe(layout(function(file) {
-      return file.frontMatter;
-    }))
+    .pipe(layout(file => file.frontMatter))
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'));
-});
+  }
+);
