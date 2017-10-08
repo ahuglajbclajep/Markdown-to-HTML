dbe8a79703eb636ccd4ac6d48a62bf47b066fef3
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-10-08 23:03:39 +0900

    output full-html

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
+var frontMatter = require('gulp-front-matter');
 var markdown = require('gulp-markdown');
+var layout = require('gulp-layout');
 
 gulp.task('default', function() {
   gulp.src('src/*.md')
+    .pipe(frontMatter({remove: true}))
     .pipe(markdown())
+    .pipe(layout(function(file) {
+      return file.frontMatter;
+    }))
     .pipe(gulp.dest('dist'));
 });
--- src/sample.md
+++ src/sample.md
@@ -1,2 +1,7 @@
+---
+title: "Markdown Sample"
+layout: "src/template.jade"
+---
+
 # Markdown Sample
 This is a sample of Markdown.
