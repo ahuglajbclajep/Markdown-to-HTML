08ab8ec7004fdf47f2b46b79ec05c98e7e8fedcc
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-02-17 11:35:15 +0900

    add CLI options

--- src/md2html.ts
+++ src/md2html.ts
@@ -6,20 +6,32 @@ import mutate from "remark-rehype";
 import doc from "rehype-document";
 import stringify from "rehype-stringify";
 import { VFile } from "vfile";
+import yargs from "yargs";
 import vfile from "to-vfile";
 
-async function md2html(markdown: VFile): Promise<VFile> {
+async function md2html(markdown: VFile, docOptions?: object): Promise<VFile> {
   const html = await unified()
     .use(parse)
     .use(mutate)
-    .use(doc)
+    .use(doc, docOptions)
     .use(stringify)
     .process(markdown);
   html.extname = ".html";
   return html;
 }
 
+const argv = yargs
+  .usage("$0 <path>... [-c <path>]")
+  .demandCommand(1)
+  .example("$0 README.md", "desc")
+  .alias("c", "css")
+  .array("c")
+  .describe("c", "CSS to include in <head> element")
+  .help()
+  .detectLocale(false)
+  .strict().argv;
+
 vfile
-  .read(process.argv[2])
-  .then((md: VFile) => md2html(md))
+  .read(argv._[0])
+  .then((md: VFile) => md2html(md, { css: argv.c }))
   .then((html: VFile) => vfile.write(html));
