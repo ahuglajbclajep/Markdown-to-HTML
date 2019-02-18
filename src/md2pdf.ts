827cec3c54d7dd69cf1c7f8f137a7e55f688ccaa
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-02-18 13:42:01 +0900

    md2pdf

--- src/md2pdf.ts
+++ src/md2pdf.ts
@@ -8,23 +8,19 @@ import mutate from "remark-rehype";
 import highlight from "rehype-highlight";
 import doc from "rehype-document";
 import stringify from "rehype-stringify";
+import puppeteer from "puppeteer-core";
 import yargs from "yargs";
 import vfile from "to-vfile";
 
-function move2cwd(source: VFile): VFile {
-  source.path = path.join(source.cwd, source.basename || "");
-  return source;
-}
-
 async function md2html(
   markdown: VFile,
-  hjs: boolean,
+  highlightjs: boolean,
   docOptions?: object
 ): Promise<VFile> {
   const html = await unified()
     .use(parse)
     .use(mutate)
-    .use(hjs ? [[highlight, { ignoreMissing: true }]] : [])
+    .use(highlightjs ? [[highlight, { ignoreMissing: true }]] : [])
     .use(doc, docOptions)
     .use(stringify)
     .process(markdown);
@@ -32,10 +28,26 @@ async function md2html(
   return html;
 }
 
+async function html2lpdf(
+  chrome: string,
+  html: VFile,
+  size: string
+): Promise<void> {
+  const browser = await puppeteer.launch({ executablePath: chrome });
+  const page = await browser.newPage();
+  await page.goto(`file:${html.path}`);
+  await page.pdf({
+    path: `${html.stem}.pdf`,
+    printBackground: true,
+    format: size as any
+  });
+  await browser.close();
+}
+
 const argv = yargs
-  .usage("$0 <path> [-h] [-c <path>] [-j <path>]")
+  .usage("$0 <path> [-h] [-c path...] [-j path...] <-p path> [-s size]")
   .demandCommand(1)
-  .example("$0 README.md", "Generate README.html")
+  .example("$0 README.md", "Generate README.pdf")
   .alias("h", "highlite")
   .boolean("h")
   .default("h", false)
@@ -46,12 +58,20 @@ const argv = yargs
   .alias("j", "js")
   .array("j")
   .describe("j", "JS to include at end of <body> element")
+  .alias("p", "path")
+  .string("p")
+  .demandOption("p")
+  .describe("p", "Path to your Chrome")
+  .alias("s", "size")
+  .default("s", "A5")
+  .describe("s", "Size of PDF")
   .help()
   .detectLocale(false).argv;
 
 (async () => {
   const md: VFile = vfile.readSync(argv._[0]);
-  move2cwd(md);
+  md.path = path.resolve(md.basename as string);
   const html = await md2html(md, argv.h, { css: argv.c, js: argv.j });
   vfile.writeSync(html);
+  await html2lpdf(argv.p, html, argv.s);
 })();
